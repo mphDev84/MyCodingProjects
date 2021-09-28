b85bbc5181308d860ce1e188474b7e2e8624be72
Matt <91234966+mphDev84@users.noreply.github.com>
2021-09-28 10:52:36 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -3,6 +3,17 @@ public class Person {
 	
 	double weight;
 	double height;
-	
+	double BMI;
 
-}
+	public Person(double w, double h) {
+		this.weight = w; 
+		this.height = h;
+		this.BMI =  w/(Math.pow(h, 2));
+	}
+
+	public double getBMI() {
+		return this.BMI;
+	}
+
+	}
+
